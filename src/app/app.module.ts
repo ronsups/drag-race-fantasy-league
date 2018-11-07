cccd518a6a4fc92729eb0893c4545755ef9276ae
ron_suprun <ron.suprun@gmail.com>
2018-11-06 22:48:49 -0500

    added select-queens to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectQueensComponent } from './select-queens/select-queens.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectQueensComponent
   ],
   imports: [
     BrowserModule,
