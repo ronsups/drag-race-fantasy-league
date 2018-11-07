dad2bb5c088f1c8799ebb4032b7034fc631691a9
ron_suprun <ron.suprun@gmail.com>
2018-11-06 22:49:04 -0500

    made queen class and mock queens list

---
+++ src/app/queen/queen.ts
@@ -0,0 +1,5 @@
+export class Queen {
+  id: number;
+  name: string;
+  url: string;
+}
---
+++ src/app/queen/queens.mock.ts
@@ -0,0 +1,7 @@
+import { Queen } from './queen';
+
+export const mockQueens: Queen[] = [
+  { id: 1, name: 'Miz Cracker', url: 'miz-cracker.jpg' },
+  { id: 2, name: 'Monét X Change', url: 'monet-x-change.jpg' },
+  { id: 3, name: 'Aquaria', url: 'aquaria.jpg' },
+];
